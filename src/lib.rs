f7fe98d568729937a3583bf017734012d90b0165
Andreas Fuchs <asf@stripe.com>
2016-12-14 11:39:15 -0800

    Split main.rs into modules, make things easier to test
    Now, everything fits on a screen, and the interfaces are a bit nicer -
    next thing, I should probably add tests for the ssh_config parser.
    (And also maybe figure out how to OAOO the config generation.)

---
+++ src/lib.rs
@@ -0,0 +1,111 @@
+pub mod ssh_config;
+pub mod known_hosts;
+
+#[macro_use] extern crate quick_error;
+
+use std::path::{Path, PathBuf};
+use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum Error {
+        KnownHostFormat(path: PathBuf, lineno: usize, line: String) {
+            // context(path: &'a Path, lineno: usize, line: &'a str)
+            //     -> (path.to_path_buf(), lineno, line.to_string())
+            display("{} line {}: {:?}", path.to_str().unwrap_or("(unprintable path)"), lineno, line)
+        }
+        NameError(name: String, protocol: String) {
+            display("{} with protocol {} would result in a bad filename", name, protocol)
+        }
+        IO(err: io::Error) {
+            from()
+                cause(err)
+                description("Couldn't read from file")
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Host {
+    name: String,
+    protocol: String,
+}
+
+impl Host {
+    pub fn new(name: &str, protocol: &str) -> Host {
+        Host{
+            name: name.to_string(),
+            protocol: protocol.to_string(),
+        }
+    }
+
+    pub fn named(name: &str) -> Host {
+        Host{
+            name: name.to_string(),
+            protocol: "ssh".to_string(),
+        }
+    }
+
+    pub fn write_bookmark(&self, dir: &Path) -> Result<(), Error> {
+        let name = format!("{} ({}).webloc", self.name, self.protocol);
+        let namepart = Path::new(&name);
+
+        let mut path = PathBuf::from(dir);
+        if namepart.is_absolute() {
+            return Err(Error::NameError(self.name.to_string(), self.protocol.to_string()));
+        }
+        path.push(namepart);
+
+        let mut bookmark_text = String::new();
+        bookmark_text.push_str(self.protocol.as_str());
+        bookmark_text.push_str("://");
+        bookmark_text.push_str(self.name.as_str());
+        let bookmark = format!(r#"<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0"><dict><key>URL</key><string>{}</string></dict></plist>
+"#,
+                               bookmark_text);
+
+        let mut f = try!(File::create(path));
+        try!(f.write_all(bookmark.as_bytes()));
+        Ok(())
+    }
+
+    pub fn ineligible(&self) -> bool {
+        self.name.contains('*') || self.name.contains('?')
+    }
+}
+
+pub trait ConfigFile<'a> {
+    fn pathname(&'a self) -> &'a Path;
+
+    fn parse_entries<R: BufRead>(&'a self, r: R) -> Result<Vec<Host>, Error>;
+
+    fn entries(&'a self) -> Result<Vec<Host>, Error> {
+        let f = try!(File::open(self.pathname()));
+        let file = BufReader::new(&f);
+        self.parse_entries(file)
+    }
+}
+
+#[test]
+fn test_host_creation() {
+    let ohai = Host::named("ohai");
+    assert_eq!(ohai.name, "ohai");
+    assert_eq!(ohai.protocol, "ssh");
+
+    let mosh_ohai = Host::new("ohai", "mosh");
+    assert_eq!(mosh_ohai.name, "ohai");
+    assert_eq!(mosh_ohai.protocol, "mosh");
+}
+
+#[test]
+fn test_host_eligibility() {
+    assert_eq!(Host::named("foo*.oink.example.com").ineligible(), true);
+    assert_eq!(Host::named("*").ineligible(), true);
+
+    assert_eq!(Host::named("foobar.oink.example.com").ineligible(), false);
+}
---
+++ src/ssh_config/mod.rs
@@ -0,0 +1,54 @@
+use ::{Host, ConfigFile, Error};
+use std::path::{Path, PathBuf};
+use std::io::prelude::*;
+
+pub struct SSHConfigFile {
+    pathname: PathBuf
+}
+
+impl SSHConfigFile {
+    pub fn new(path: &Path) -> SSHConfigFile {
+        SSHConfigFile{
+            pathname: path.to_path_buf()
+        }
+    }
+}
+
+impl<'a> ConfigFile<'a> for SSHConfigFile {
+    fn pathname(&'a self) -> &'a Path {
+        self.pathname.as_path()
+    }
+
+    fn parse_entries<R: BufRead>(&'a self, file: R) -> Result<Vec<Host>, Error> {
+        let mut hosts: Vec<Host> = vec!();
+        for maybe_line in file.lines() {
+            let line = try!(maybe_line);
+
+            let line = line.trim();
+            // Skip comments or blank lines:
+            if line.len() == 0 || line.starts_with('#') {
+                continue;
+            }
+
+            let mut protocols: Vec<&str> = vec!["ssh"];
+            let annotated: Vec<&str> = line.split("#:").collect();
+            if annotated.len() > 1 {
+                protocols = annotated[1].split(",").collect();
+            }
+
+            if annotated[0].to_lowercase().starts_with("host") {
+                let host_entries: Vec<&str> = annotated[0].split_whitespace().skip(1).collect();
+                for proto in protocols.iter() {
+                    hosts.extend(host_entries.as_slice().into_iter().map(|name| Host::new(name, proto)))
+                }
+            }
+        }
+        Ok(hosts)
+    }
+}
+
+#[test]
+fn test_ssh_config() {
+    let c = SSHConfigFile::new(Path::new("/tmp"));
+    assert_eq!(c.pathname(), Path::new("/tmp"));
+}
